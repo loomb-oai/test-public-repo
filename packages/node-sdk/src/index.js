e5c225ea903ddd8d3f1050eddee1c0874b62c6aa
Daniel Loomb <loomb@openai.com>
2026-05-20 13:47:47 -0700

    feat(samples): expose get_magic_number demo API

--- packages/node-sdk/src/index.js
+++ packages/node-sdk/src/index.js
@@ -1,13 +1,6 @@
 const SDK_VERSION = "0.1.0";
+const MAGIC_NUMBER = 7;
 
-export function node_gday(name = "release-action") {
-  return `g'day ${name} from the demo Node SDK`;
-}
-
-export function node_goodbye(name = "release-action") {
-  return `hooroo ${name} from the demo Node SDK`;
-}
-
-export function get_sdk_version() {
-  return SDK_VERSION;
+export function get_magic_number() {
+  return MAGIC_NUMBER;
 }
